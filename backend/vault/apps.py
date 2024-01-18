d6d569485aaa8cb15a31fa2c0c7ba30a90e33919
ArslanTu <arslantu@arslantu.xyz>
2024-01-18 22:14:49 +0800

    add verbose_name to app 'vault'

--- backend/vault/apps.py
+++ backend/vault/apps.py
@@ -1,6 +1,17 @@
+# -*- coding: utf-8 -*-
+"""A vault app for subjects like books, movies, etc
+
+@File    : apps.py
+@Time    : 2024-01-18, Thu, 22:9
+@Author  : ArslanTu
+@Mail    : arslantu@arslantu
+
+"""
+
 from django.apps import AppConfig
 
 
 class VaultConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'vault'
+    verbose_name = '内容库'
